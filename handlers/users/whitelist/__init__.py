97f0070cb8108d52d5255102dbc866444d4fccc5
Nikita <nikita.gubarev31@gmail.com>
2024-10-28 17:17:22 +0200

    Create whitelist menu handler and router

--- handlers/users/whitelist/__init__.py
+++ handlers/users/whitelist/__init__.py
@@ -1,4 +1,8 @@
 from aiogram import Router
 
+from .menu import router as menu_router
+
 
 whitelist_router = Router()
+
+whitelist_router.include_routers(menu_router)
---
+++ handlers/users/whitelist/menu.py
@@ -0,0 +1,24 @@
+from aiogram import Router, F
+from aiogram.types import Message
+
+from utils.user_manager import UserManager
+from keyboards.whitelist import get_whitelist_menu_keyboard
+
+
+router = Router()
+
+
+@router.message(F.text.lower() == "🔘 whitelist")
+async def handle_whitelist_menu(message: Message):
+    """Handles whitelist menu."""
+    user_status = await UserManager.get_user_status(
+        user_id=message.from_user.id
+    )
+    if user_status == "demo":
+        return await message.answer(
+            "❌ Ця функція доступна тільки у платній версії боту."
+        )
+    await message.answer(
+        "Вітаю у меню вайтлисту! Виберіть дію:",
+        reply_markup=await get_whitelist_menu_keyboard(),
+    )
