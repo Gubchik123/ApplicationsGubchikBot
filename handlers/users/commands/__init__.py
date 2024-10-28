8f0e32cf7eaa4203014e6e5650f2edb056b1632f
Nikita <nikita.gubarev31@gmail.com>
2024-10-28 13:46:42 +0200

    Create menu command handler and router

--- handlers/users/commands/__init__.py
+++ handlers/users/commands/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 
 from .start import router as start_router
+from .menu import router as menu_router
 
 
 commands_router = Router()
---
+++ handlers/users/commands/menu.py
@@ -0,0 +1,17 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from utils.decorators import clear_state_before
+
+from ..menu import handle_menu
+
+
+router = Router()
+
+
+@router.message(Command("menu"))
+@clear_state_before
+async def handle_menu_command(message: Message):
+    """Handles the /menu command."""
+    await handle_menu(message)
