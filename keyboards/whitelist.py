90fccae58444501f2d858e26da8e96c66e6b5a26
Nikita <nikita.gubarev31@gmail.com>
2024-10-29 12:16:48 +0200

    Add make_domains_keyboard
    into the keyboards/whitelist.py

--- keyboards/whitelist.py
+++ keyboards/whitelist.py
@@ -17,6 +17,21 @@ async def get_whitelist_menu_keyboard() -> ReplyKeyboardMarkup:
 async def get_back_whitelist_menu_keyboard() -> ReplyKeyboardMarkup:
     """Returns back whitelist menu keyboard."""
     return ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="Повернутися до меню вайтлиста")]],
+        keyboard=[[get_back_whitelist_button()]],
         resize_keyboard=True,
+    )
+
+
+def get_back_whitelist_button() -> KeyboardButton:
+    """Returns back whitelist button."""
+    return KeyboardButton(text="Повернутися до меню вайтлиста")
+
+
+async def make_domains_keyboard(domains: list[str]) -> ReplyKeyboardMarkup:
+    """Returns made domains keyboard."""
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text=domain)] for domain in domains]
+        + [[get_back_whitelist_button()]],
+        resize_keyboard=True,
+        one_time_keyboard=True,
     )
