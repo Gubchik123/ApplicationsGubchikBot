3745832e1af1fbfed318445bcd99203b85cabdcd
Nikita <nikita.gubarev31@gmail.com>
2024-10-30 12:38:30 +0200

    Fix the applications and whitelist command handlers to include state parameter

--- handlers/users/commands/whitelist.py
+++ handlers/users/commands/whitelist.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
 
 from utils.decorators import clear_state_before
 
@@ -12,6 +13,6 @@ router = Router()
 
 @router.message(Command("whitelist"))
 @clear_state_before
-async def handle_whitelist_command(message: Message):
+async def handle_whitelist_command(message: Message, state: FSMContext):
     """Handles the /whitelist command."""
-    await handle_whitelist_menu(message)
+    await handle_whitelist_menu(message, state)
